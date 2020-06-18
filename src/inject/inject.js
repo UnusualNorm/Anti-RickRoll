32823681f35f375b2a7ac4f23b843a24b5bb9af2
UnusualN0rm <40722525+UnusualN0rm@users.noreply.github.com>
2020-06-18 16:40:50 -0400

    Update inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,23 +1,26 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Hello. This message was sent from scripts/inject.js");
+			// ----------------------------------------------------------
 
-		var links = ["dQw4w9WgXcQ", "42OleX0HR4E"];
-		for (var i=0; i < links.length; i++) {
-			$(document).on('click', 'a[href*=' + links[i] + ']', function(e){
-				if (!confirm('Are you sure?  You are about to get Rick Rolled. :/')) e.preventDefault();
-			});
-			$('a[href*=' + links[i] + ']').append(' <div class="rickrollbox"><img src="' + chrome.extension.getURL('icons/rickastley.png') + '" width="24"></div>');
-			if (window.location.href.indexOf(links[i]) >= 0) {
-				alert("You've been rick rolled. :'(");
+			var links = ["dQw4w9WgXcQ", "42OleX0HR4E"];
+			for (var i=0; i < links.length; i++) {
+				$(document).on('click', 'a[href*=' + links[i] + ']', function(e){
+					if (!confirm('Are you sure?  You are about to get Rick Rolled. :/')) e.preventDefault();
+				});
+				$('a[href*=' + links[i] + ']').append(' <div class="rickrollbox"><img src="' + chrome.extension.getURL('icons/rickastley.png') + '" width="24"></div>');
+				if (window.location.href.indexOf(links[i]) >= 0) {
+					var r = confirm("You are being rick rolled, but there is hope! press OK to dodge the blow and be redirected.");
+					if (r == true) {
+						top.location.href = chrome.extension.getURL('icons/rickastley.png');
+					}
+				}
 			}
 		}
-	}
 	}, 10);
 });
