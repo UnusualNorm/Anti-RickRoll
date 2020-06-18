7f67a8e4823816cc3af217a3a8848962890b6a31
UnusualN0rm <40722525+UnusualN0rm@users.noreply.github.com>
2020-06-18 16:11:56 -0400

    i'm dying inside trying to fix this
    trying to have it stop direct links, jquery doesnt load fast enough

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -11,9 +11,12 @@ chrome.extension.sendMessage({}, function(response) {
 		var links = ["dQw4w9WgXcQ", "42OleX0HR4E"];
 		for (var i=0; i < links.length; i++) {
 			$(document).on('click', 'a[href*=' + links[i] + ']', function(e){
-				if (!confirm('Are you sure?  You are about to get Rick Rolled')) e.preventDefault();
+				if (!confirm('Are you sure?  You are about to get Rick Rolled. :/')) e.preventDefault();
 			});
 			$('a[href*=' + links[i] + ']').append(' <div class="rickrollbox"><img src="' + chrome.extension.getURL('icons/rickastley.png') + '" width="24"></div>');
+			if (window.location.href.indexOf(links[i]) >= 0) {
+				alert("You've been rick rolled. :'(");
+			}
 		}
 	}
 	}, 10);
